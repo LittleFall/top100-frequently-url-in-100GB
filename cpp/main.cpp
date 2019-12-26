8d7a6b4a34d535752779f8a19482de15d33c758a
LittleFall <1578166061@qq.com>
2019-12-27 06:35:19 +0800

    cpp程序，整理程序结构

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,7 +5,6 @@ const string data_path = "../data_finder/data100mb.txt";
 const string block_path = "../solver/block";
 const unsigned radix = 31, blocks = 131, limit = 100;
 
-unsigned count[blocks];
 unsigned hash(const string &url) //获得url的hash值，范围是[0,blocks-1]
 {
 	unsigned res = 0;
@@ -13,63 +12,87 @@ unsigned hash(const string &url) //获得url的hash值，范围是[0,blocks-1]
 		res = (res * radix + unsigned(ch)) % blocks;
 	return res;
 }
-std::ofstream ofs[blocks];
-void init()
+
+std::vector<unsigned> classify_to_blocks()
 {
-	for(unsigned i=0; i<blocks; ++i)
-		ofs[i] = std::ofstream(block_path + std::to_string(i));
-}
-int main(void)
-{
-	clock_t t_st = clock();
-	init();
 	std::ifstream fin(data_path);
+	std::vector<std::ofstream> ofs(blocks);
+	for(unsigned block_id=0; block_id<blocks; ++block_id)
+		ofs[block_id] = std::ofstream(block_path + std::to_string(block_id));
 
 	string url;
+	std::vector<unsigned> block_count(blocks);
 	while(getline(fin, url))
 	{
 		unsigned hash_val = hash(url);
 		ofs[hash_val] << url << "\n";
-		++count[hash_val];
+		++block_count[hash_val];
 	}
-	printf("classify cost = %.2fs\n",double(clock()-t_st)/CLOCKS_PER_SEC );
 
+	fin.clear();
+	for(unsigned block_id=0; block_id<blocks; ++block_id)
+		ofs[block_id].clear();
+	return block_count;
+}
+
+std::vector<std::pair<string, unsigned>> find_top_from_blocks()
+{
 	using psu = std::pair<string, unsigned>;
 	auto big_top = [](psu &a, psu &b){return a.second < b.second;};
 	auto little_top = [](psu &a, psu &b){return a.second > b.second;};
-	std::vector<psu> total_heap(limit+1);
 
-	make_heap(total_heap.begin(), total_heap.end(), little_top);
+	std::vector<psu> total_heap(limit, std::make_pair("",0)); // global max 100, little_top heap
+
 	for(unsigned i=0; i<blocks; ++i)
 	{
 		std::ifstream fin(block_path + std::to_string(i));
 		string url;
 		std::unordered_map<string, unsigned> counter;
+
 		while(getline(fin, url))
 			++counter[url];
 
-		std::vector<psu> heap(counter.begin(), counter.end());
-
+		std::vector<psu> heap(counter.begin(), counter.end()); // big_top heap
 		std::make_heap(heap.begin(), heap.end(), big_top);
-		for(unsigned j=0; j<std::min(limit, (unsigned)heap.size()); ++j)
+
+		for(unsigned j=0, lim = std::min(limit, (unsigned)heap.size()); 
+			j<lim; ++j)
 		{
 			if(heap.front().second <= total_heap.front().second) break;
-			total_heap.back() = heap.front(); //新来一个放在100处
-			push_heap(total_heap.begin(), total_heap.end(), little_top); //插入堆，最小一个在0处
-			pop_heap(total_heap.begin(), total_heap.end(), little_top); //把最小的放在100处
+			pop_heap(total_heap.begin(), total_heap.end(), little_top); // pop min to back
+			total_heap.back() = heap.front(); // update back
+			push_heap(total_heap.begin(), total_heap.end(), little_top); // push back into heap
 
-			//printf("%d ",heap.front().second);
-			std::pop_heap(heap.begin(), heap.end(), big_top);
-			heap.pop_back();
+			std::pop_heap(heap.begin(), heap.end(), big_top); 
+			heap.pop_back(); 
 		}
+		fin.close();
 	}
-	//printf("\n");
-	total_heap.pop_back();
-	sort(total_heap.begin(), total_heap.end(), little_top);
-	// for(auto p:total_heap)
-	// 	printf("%d %s\n", p.second, p.first.c_str() );
-	// printf("\n");
+	
+	sort(total_heap.begin(), total_heap.end(), little_top); 
+	return total_heap;
+}
 
-	printf("total cost = %.2fs\n",double(clock()-t_st)/CLOCKS_PER_SEC );
+int main(void)
+{
+	clock_t t_st = clock();
+
+	printf("start.\n");
+	printf("blocks = %d, hash radix = %d, find top %d\n\n", blocks, radix, limit );
+
+	std::vector<unsigned> block_count = classify_to_blocks();
+	printf("classify end.\n");
+	printf("total urls: %u\n", std::accumulate(block_count.begin(), block_count.end(), 0u) );
+	printf("ave urls of block: %u\n", std::accumulate(block_count.begin(), block_count.end(), 0u)/blocks );
+	printf("max urls of block: %u\n", *std::max_element(block_count.begin(), block_count.end()));
+	printf("classify time cost = %.2fs\n\n",double(clock()-t_st)/CLOCKS_PER_SEC );
+
+	std::vector<std::pair<string, unsigned>> top = find_top_from_blocks();
+	printf("well done.\n");
+	for(auto psu:top)
+		printf("%d %s\n", psu.second, psu.first.c_str() );
+	printf("\n");
+	printf("total time cost = %.2fs\n",double(clock()-t_st)/CLOCKS_PER_SEC );
+
     return 0;
 }
