579f59990b579195c7afcc7f165c3038735d0261
LittleFall <1578166061@qq.com>
2019-12-30 18:53:55 +0800

    打开O2，优化随机数据生成器，生成100mb数据需要1.3s

--- src/random_url.cpp
+++ src/random_url.cpp
@@ -1,6 +1,5 @@
 /*
 random_url.cpp 生成随机url
-http
 */
 #include<bits/stdc++.h>
 using std::string;
@@ -8,61 +7,55 @@ using std::vector;
 
 /* 顶级域名 */
 const vector<string> TLDs = {
-	"top", "com", "cn", "xyz", "xin", "vip", 
-	"red", "net", "org", "wang", "gov", "edu", 
-	"mil", "co", "biz", "name", "info", "mobi", 
-	"pro", "travel", "club", "museum", "int", "aero", 
-	"post", "rec", "asia", "win"
-};
+	"top", "com", "xyz", "xin", "vip", "red", "net", "org", 
+	"wang", "gov", "edu", "mil", "co", "biz", "name", "info", 
+	"mobi", "pro", "travel", "club", "museum", "int", "aero", "post",
+	"rec", "asia", "win", "cn", "fr", "us", "uk", "ru"
+}; 
 /* 协议 */
 const vector<string> schemes = {
-	"http", "https", "ftp"
+	"http", "https", "https", "ftp"
 };
 /* 字符表 */
-const string alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"; // no '-'
-
-const unsigned string_len_limit = 10;
-const unsigned path_layer_limit = 5;
-
+const string alphabet = "0123456789abcdefghijklmnopqrstuv"; // no "-wxyz", len 32
 
 std::mt19937 rand_gen(time(0));
+inline string random_scheme(){return schemes[rand_gen()&3];}
+inline string random_TLD(){return TLDs[rand_gen()&31];}
+inline char random_url_char(){return alphabet[rand_gen()&31];}
+inline unsigned random_len_limit(){return (rand_gen()&7)+1;} 
+inline unsigned random_path_layer(){return (rand_gen()&3)+1;}
+
 inline string random_string()
 {
 	string str;
-	unsigned len = rand_gen()%string_len_limit;
+	unsigned len = random_len_limit();
 	for(unsigned i=0; i<len; ++i)
-		str.push_back(alphabet[rand_gen()%alphabet.size()]);
+		str.push_back(random_url_char());
 	return str;
 }
 
-template<typename Iter>
-inline Iter select_randomly(Iter start, Iter end) {
-	return start + rand_gen()%(end-start);
-}
-
-
+string buf;
 int main(void)
 {
 	clock_t t_st = clock();
 
 	std::ofstream fout("../data/data100mb_rand.txt");
-	int total_byte_last = 100*1024*1024; //don't use unsigned
+	unsigned long long total_byte_last = 100*1024*1024; 
 
-
-	while(total_byte_last>0)
+	while(buf.size() < total_byte_last)
 	{
-		string url;
-		url += *select_randomly(schemes.begin(), schemes.end());
-		url += "://";
-		url += random_string() + "." + random_string() + ".";
-		url += *select_randomly(TLDs.begin(), TLDs.end());
-		unsigned path_layer = rand_gen() % path_layer_limit;
+		buf += random_scheme() + "://";
+		buf += random_string() + "." + random_string() + ".";
+		buf += random_TLD();
+
+		unsigned path_layer = random_path_layer();
 		while(path_layer--)
-			url += "/" + random_string();
-		fout << url << "\n";      
-		total_byte_last -= url.size();
-		//printf("%d\n",total_byte_last );
+			buf += "/" + random_string();
+
+		buf += "\n";
 	}
+	fout << buf << "\n";      
 
 	printf("%.2fs\n",double(clock()-t_st)/CLOCKS_PER_SEC );
 
